ef9aba6c69115dcbcc72ee4b92f95b499a6e565a
Andrea Manzo <andream041@gmail.com>
2015-07-25 21:16:30 -0700

    Added fndds data into database

---
+++ fndds.sql
@@ -0,0 +1,42 @@
+
+-- DROP TABLE main_food_desc;
+-- CREATE TABLE main_food_desc(
+    -- foodcode int PRIMARY KEY, 
+    -- start_date date,
+    -- end_date date,
+    -- main_food_desc varchar(200),
+    -- fortification_id int,
+    -- empty varchar(1)
+-- );
+
+-- \copy main_food_desc from '/home/andrea/Documents/calcount/fndds/MainFoodDesc.txt' (delimiter '^');
+
+
+-- DROP TABLE nut_vals;
+-- CREATE TABLE nut_vals(
+    -- foodcode int,
+    -- nutcode int,
+    -- start_date date,
+    -- end_date date,
+    -- nutval numeric,
+    -- empty varchar(1)
+-- );
+
+-- \copy nut_vals from '/home/andrea/Documents/calcount/fndds/FNDDSNutVal.txt' (delimiter '^');
+
+
+-- DROP TABLE nut_desc;
+-- CREATE TABLE nut_desc(
+    -- nutcode int,
+    -- nut_desc varchar(45),
+    -- tagname varchar(15),
+    -- unit varchar(10),
+    -- decimals int,
+    -- empty varchar(1)
+-- );
+
+-- \copy nut_desc from '/home/andrea/Documents/calcount/fndds/NutDesc.txt' (delimiter '^');
+
+SELECT main_food_desc, nut_desc, nutval FROM nut_vals INNER JOIN nut_desc using (nutcode) INNER JOIN main_food_desc USING (foodcode) WHERE nut_desc = '~Energy~' ORDER BY nutval;
+
+
